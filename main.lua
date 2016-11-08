daeefc1c488fc9f0d5784f4ffe585f6fa75dbc5b
twobit <s8572327@gmail.com>
2016-11-08 21:53:00 +0100

    exodus

--- main.lua
+++ main.lua
@@ -1,5 +1,19 @@
+G = love.graphics
+
+
+-- init technical stuff
+W = 320
+H = 200
+G.setDefaultFilter("nearest", "nearest")
+canvas = G.newCanvas(W, H)
+love.window.setMode(W * 2, H * 2, {resizable = true})
+love.mouse.setVisible(false)
+
+
 require("helper")
 require("map")
+
+
 
 map = Map()
 
@@ -16,9 +30,39 @@ function love.update(dt)
 
 end
 
+t = 0
 
 function love.draw()
+	G.setCanvas(canvas)
+	G.clear(0, 0, 0)
+
+
+	-- render stuff
+
+	t = t + 1
+
+
+--	G.translate(400, 300)
+	G.rotate(math.sin(t*0.1) * 0.2)
+
+
+	map:draw()
 
 
 
+
+
+	-- draw canvas independent of resolution
+	local w = G.getWidth()
+	local h = G.getHeight()
+	G.origin()
+	if w / h < W / H then
+		G.translate(0, (h - w / W * H) * 0.5)
+		G.scale(w / W, w / W)
+	else
+		G.translate((w - h / H * W) * 0.5, 0)
+		G.scale(h / H, h / H)
+	end
+	G.setCanvas()
+	G.draw(canvas)
 end
--- map.lua
+++ map.lua
@@ -1,20 +1,62 @@
 local json = require("dkjson")
 
 
-
 Map = Object:new()
 function Map:init()
-
 
 	local raw = love.filesystem.read("data/map.json")
 	local data = json.decode(raw)
 
 
+	self.tileset = G.newImage("data/tileset.png")
+	self.quads = makeQuads(self.tileset:getWidth(), self.tileset:getHeight(), 16)
+	self.w = data.width
+	self.h = data.height
+
+
+	self.entities = {}
+	self.layers = {}
+
 	for i, layer in pairs(data.layers) do
-		print(layer.name, layer.type)
+
+
+		if layer.type == "tilelayer" then
+
+			self.layers[layer.name] = layer.data
+
+
+
+
+		elseif layer.name == "entities" then
+			for j, obj in ipairs(layer.objects) do
+
+
+				if obj.name == "player" then
+--					entities.player = {
+--						x = obj.x + obj.width / 2
+--						y = obj.y + obj.height / 2
+--					}
+				end
+
+			end
+		end
 
 	end
 
 end
+function Map:draw()
+
+	for y = 0, 30 do
+		for x = 0, 30 do
+
+			local cell = self.layers.walls[y * self.w + x + 1]
+			if cell > 0 then
+
+				G.draw(self.tileset, self.quads[cell], x * 16, y * 16)
+
+			end
 
 
+		end
+	end
+end
